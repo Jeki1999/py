99f215f62a22e7dc62003a09f3b838da61fcbdac
Евгений В.А <evgeniyv.a.@Mac-mini-Evgenij.local>
2022-06-17 17:49:07 +0600

    3.2

--- 11.py
+++ 11.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -22,33 +25,28 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
-        
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
 
 
 class Lecturer(Mentor):
-    super().__init__()
+    def __init__(self,name,surname):
+        self.name = name
+        self.surname = surname
+        self.grades = {}
+        summ = []
+        count = 0
+
     def __str__(self):
-        return f"Name: {self.__name}, Surname: {self.__surname}" 
+        return f"Имя: {self.name}" + '\n' + f"Фамилия: {self.surname}" #clear + "\n" + f"Средняя оценка за лекции: {self.average_rating}"
 
     def average_rating(self):
+        for grade in self.grades:
+            summ += grade
+            count += 1
+        return summ / count
 
 class Reviewer(Mentor):
-    super().__init__()
-
     def __str__(self):
-        return f"Name: {self.__name}, Surname: {self.__surname}" 
+        return f"Имя: {self.name}" + '\n' + f"Фамилия: {self.surname}" 
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -58,3 +56,17 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+        
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_lecturer = Lecturer('Eugeniush', 'Veselskiy')
+some_reviewer = Reviewer('Some', 'Buddy')
+cool_lecturer.grades = 
+
+#best_student.rate_hw('cool_metr')
+print(cool_lecturer)
